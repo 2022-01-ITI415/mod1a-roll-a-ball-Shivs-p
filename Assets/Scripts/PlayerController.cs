97641e520d99598b746c08174fa604f65a00ce33
Shivani Patel <97494145+Shivs-p@users.noreply.github.com>
2022-01-31 15:49:19 -0500

    4.1-6.5

--- Assets/Scripts/PlayerController.cs
+++ Assets/Scripts/PlayerController.cs
@@ -33,4 +33,11 @@ public class PlayerController : MonoBehaviour
         rb.AddForce(movement * speed);
     }
 
+    private void OnTriggerEnter(Collider other)
+    {
+        if (other.gameObject.CompareTag("Pick Up"))
+        {
+            other.gameObject.SetActive(false);
+        }
+    }
 }
